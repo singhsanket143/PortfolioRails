6e6a886880d99288392b0a58ebe6ee49ec26b4d0
Sanket Singh <singhsanket143@gmail.com>
2018-05-11 12:57:49 +0530

    new and create method created for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,21 @@
 class PortfoliosController < ApplicationController
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html {redirect_to portfolios_path, notice: 'Your portfolio has been created'}
+      else
+        fromart.html {redirect_to :new}
+      end
+    end
+  end
 end
