c79b0f898934f828aefb2525ade6abf9b4f4fefe
Sanket Singh <singhsanket143@gmail.com>
2018-05-11 13:12:28 +0530

    edit method created

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,4 +18,19 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html {redirect_to portfolios_path, notice: 'Your portfolio has been updated'}
+      else
+        fromart.html {redirect_to :edit}
+      end
+    end
+  end
 end
