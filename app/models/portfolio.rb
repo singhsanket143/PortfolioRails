78dbd0f0e6c9922da4e49425f82285340ab4c6d3
Sanket Singh <singhsanket143@gmail.com>
2018-05-14 03:00:23 +0530

    Integrated defaults portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,12 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails, -> {where(subtitle: 'Ruby on rails')}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
+
