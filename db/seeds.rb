4908f92b300aa6f61fab776a18f6bbfd47d446b7
Sanket Singh <singhsanket143@gmail.com>
2018-05-14 11:18:46 +0530

    technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,11 @@ puts "8 portfolio created"
       thumb_image: "http://placehold.it/350x200"
   )
 end
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+                name: "Technology #{technology}",
+  )
+end
+
+puts "3 technology created"
