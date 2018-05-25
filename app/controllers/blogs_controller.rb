68036fb077a74de223dcb0f09719420cc4d23851
Sanket Singh <singhsanket143@gmail.com>
2018-05-26 03:51:35 +0530

    Implemented partials for blogs font awesome and pagination with kaminari

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(5)
   end
 
   # GET /blogs/1
