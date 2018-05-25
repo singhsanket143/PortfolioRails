4f45cd3b0b4b21f59290244a019efc6a6d956b70
Sanket Singh <singhsanket143@gmail.com>
2018-05-25 09:28:31 +0530

    Implemented authorization using petergate

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,6 +1,14 @@
 module CurrentUserConcern
   extend ActiveSupport::Concern
   def current_user
-    super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+    super || guest_user
+  end
+
+  def guest_user
+    guest = GuestUser.new
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    guest
   end
 end
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:show, :update, :destroy, :edit]
   layout "portfolio"
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   def index
     # @portfolio_items = Portfolio.angular
