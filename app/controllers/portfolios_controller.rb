81472bfa16e5079093247bb2bbe1012b10579875
Sanket Singh <singhsanket143@gmail.com>
2018-05-30 20:26:24 +0530

    Linked drag and drop with backend

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,6 +9,13 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.by_position
   end
 
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+    render nothing: true
+  end
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
